09c726e37d81b0f51444318bb2c229bbbd9008ce
toms74209200 <motomotomato@gmail.com>
2021-06-14 01:18:16 +0900

    [add]add client service

--- client/src/main/java/com/example/spring_grpc_sample/client/service/impl/HelloServiceImpl.java
+++ client/src/main/java/com/example/spring_grpc_sample/client/service/impl/HelloServiceImpl.java
@@ -2,12 +2,15 @@ package com.example.spring_grpc_sample.client.service.impl;
 
 import com.example.spring_grpc_sample.client.service.HelloService;
 import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,6 +22,13 @@ public class HelloServiceImpl implements HelloService {
 
     @Override
     public Optional<String> receiveGreeting(String name) {
-        return Optional.empty();
+        log.info("name: {}", name);
+        HelloRequest request = HelloRequest.newBuilder()
+                .setName(name)
+                .build();
+        HelloReply reply = helloGreeterStub.withDeadlineAfter(1, TimeUnit.SECONDS).sayHello(request);
+        String message = reply.getMessage();
+        log.info("message: {}", message);
+        return Optional.ofNullable(message);
     }
 }
--- client/src/test/java/com/example/spring_grpc_sample/client/service/impl/HelloServiceImplTest.java
+++ client/src/test/java/com/example/spring_grpc_sample/client/service/impl/HelloServiceImplTest.java
@@ -1,8 +1,6 @@
 package com.example.spring_grpc_sample.client.service.impl;
 
 import com.example.spring_grpc_sample.client.service.HelloService;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.examples.helloworld.GreeterGrpc;
@@ -10,8 +8,6 @@ import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
 import io.grpc.stub.StreamObserver;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,13 +17,11 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.spy;
 
 @SpringBootTest
 public class HelloServiceImplTest {
 
     private static final int SERVER_PORT = 8080;
-    private static final String SERVER_HOST = "localhost";
 
     @Autowired
     private HelloService helloService;
@@ -35,8 +29,9 @@ public class HelloServiceImplTest {
     private Server serverMock;
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown() throws Exception {
         serverMock.shutdown();
+        serverMock.awaitTermination();
     }
 
     @Test
@@ -54,6 +49,8 @@ public class HelloServiceImplTest {
 
         startServer(greeterMock);
 
+        Thread.sleep(1000);
+
         String expectName = "test";
         String actualReply = helloService.receiveGreeting(expectName).orElse(null);
 
@@ -64,11 +61,13 @@ public class HelloServiceImplTest {
     }
 
     @Test
-    public void testReceiveGreetingNoReply() {
+    public void testReceiveGreetingNoReply() throws Exception {
         GreeterGrpc.GreeterImplBase greeterMock = new GreeterGrpc.GreeterImplBase() {
             @Override
             public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {}
         };
+
+        startServer(greeterMock);
 
         assertThrows(
                 Exception.class,

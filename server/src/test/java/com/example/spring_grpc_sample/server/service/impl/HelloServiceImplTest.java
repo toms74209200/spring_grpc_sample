e056411cfe7a3bb1ae98603fc52315d53bbcedf0
toms74209200 <motomotomato@gmail.com>
2021-10-09 21:55:52 +0900

    [clean]format code

--- server/src/test/java/com/example/spring_grpc_sample/server/service/impl/HelloServiceImplTest.java
+++ server/src/test/java/com/example/spring_grpc_sample/server/service/impl/HelloServiceImplTest.java
@@ -1,5 +1,8 @@
 package com.example.spring_grpc_sample.server.service.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
 import io.grpc.stub.StreamObserver;
@@ -8,31 +11,24 @@ import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
 @SpringBootTest
 public class HelloServiceImplTest {
 
-    @Autowired
-    private HelloServiceImpl helloService;
+    @Autowired private HelloServiceImpl helloService;
 
     @Test
     public void testSayHelloVerifyRequest() throws Exception {
         String expectName = "Foo";
-        HelloRequest request = HelloRequest.newBuilder()
-                .setName(expectName)
-                .build();
-        @SuppressWarnings("unchecked") StreamObserver<HelloReply> responseObserverMock = mock(StreamObserver.class);
+        HelloRequest request = HelloRequest.newBuilder().setName(expectName).build();
+        @SuppressWarnings("unchecked")
+        StreamObserver<HelloReply> responseObserverMock = mock(StreamObserver.class);
 
         helloService.sayHello(request, responseObserverMock);
 
         ArgumentCaptor<HelloReply> replyArgumentCaptor = ArgumentCaptor.forClass(HelloReply.class);
         verify(responseObserverMock).onNext(replyArgumentCaptor.capture());
         HelloReply actualReply = replyArgumentCaptor.getValue();
-        assertThat(actualReply.getMessage())
-                .contains("Hello")
-                .contains(expectName);
+        assertThat(actualReply.getMessage()).contains("Hello").contains(expectName);
 
         verify(responseObserverMock).onCompleted();
     }
